6ae6ea7c28d813e7c142fad5d7af12e00bcafa7a
sogrey <suzhaoheng2006@163.com>
2022-05-14 01:36:18 +0800

    add .

---
+++ vite-vue3-cherry-markdown/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Cherry from 'cherry-markdown/dist/cherry-markdown.core';
+const cherryInstance = new Cherry({
+  id: 'markdown-container',
+  value: '# welcome to cherry editor!',
+});
+</script>
+
+<template>
+  <div id="markdown-container"></div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+#markdown-container{
+    width:100%;height:100%;padding:0;margin:0;
+}
+</style>
---
+++ vite-vue3-markdown-editor/src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import MarkDownEditor from './components/MarkDownEditor.vue'
+export default {
+    name: 'HelloWorld',
+    components: {
+        MarkDownEditor
+    },
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+<template>
+ <div id="app">
+        <div class="root_container">
+            <hello-mark-down>            </hello-mark-down>
+        </div>
+ </div>
+</template>
+
+<style>
+#app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;  width: 100%;
+  height: 100%;
+}
+
+.root_container {}
+</style>
